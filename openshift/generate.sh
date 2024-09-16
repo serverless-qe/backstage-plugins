7aab0d408d90345172d8bc3e9d355c5a6e5fff0a
Ali Ok <aliok@redhat.com>
2024-09-16 13:52:59 +0300

    Update generate.sh

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
-GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
+GOFLAGS='' go install github.com/openshift-knative/hack/cmd/generate@latest
+
 
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
